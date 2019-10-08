47dd166aa54eee22e7c478d3d3f312636a1f440e
Vincent Bernardoff <vb@luminar.eu.org>
2019-10-08 21:47:43 +0800

    fix compilation (logs)

--- test/ws.ml
+++ test/ws.ml
@@ -48,7 +48,7 @@ let cmd =
   Command.async ~summary:"BFX shell" begin
     let open Command.Let_syntax in
     [%map_open
-      let () = Logs_async_reporter.set_level_via_param None in
+      let () = Logs_async_reporter.set_level_via_param [] in
       fun () ->
         Logs.set_reporter (Logs_async_reporter.reporter ()) ;
         main ()
