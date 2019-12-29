dfac0b74d2cda6ce4e25301155cd42372032c372
Vincent Bernardoff <vb@luminar.eu.org>
2019-12-29 17:01:11 +0100

    async: remove Persistent.create'

--- src/bfx_ws_async.ml
+++ src/bfx_ws_async.ml
@@ -4,11 +4,5 @@ open Bfx_ws
 let of_string ?buf msg = Yojson_encoding.destruct_safe encoding (Yojson.Safe.from_string ?buf msg)
 let to_string ?buf cmd = Yojson.Safe.to_string ?buf (Yojson_encoding.construct encoding cmd)
 
-module Persistent = struct
-  module Conn = (Fastws_async.MakePersistent (struct type r = Bfx_ws.t type w = Bfx_ws.t end))
-  include Persistent_connection_kernel.Make(Conn)
-
-  let create' ~server_name ?on_event ?retry_delay ?buf =
-    create ~server_name ?on_event ?retry_delay
-      ~connect:(Fastws_async.connect ~wr:(to_string ?buf) ~rd:(of_string ?buf))
-end
+module Conn = (Fastws_async.MakePersistent (struct type r = Bfx_ws.t type w = Bfx_ws.t end))
+module Persistent = Persistent_connection_kernel.Make(Conn)
--- src/bfx_ws_async.mli
+++ src/bfx_ws_async.mli
@@ -1,18 +1,6 @@
-open Core
-open Async
-
 val of_string : ?buf:Bi_outbuf.t -> string -> Bfx_ws.t
 val to_string : ?buf:Bi_outbuf.t -> Bfx_ws.t -> string
 
-module Persistent : sig
-  include Persistent_connection_kernel.S
-    with type address = Uri.t
-     and type conn = (Bfx_ws.t, Bfx_ws.t) Fastws_async.t
-
-  val create' :
-    server_name:string ->
-    ?on_event:(Event.t -> unit Deferred.t) ->
-    ?retry_delay:(unit -> Time_ns.Span.t) ->
-    ?buf:Bi_outbuf.t ->
-    (unit -> address Or_error.t Deferred.t) -> t
-end
+module Persistent : Persistent_connection_kernel.S
+  with type address = Uri.t
+   and type conn = (Bfx_ws.t, Bfx_ws.t) Fastws_async.t
